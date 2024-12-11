698b7da5a124d63ba06f2506917aeb6d9b436261
tu-usuario <afprietol@eafit.edu.co>
2024-12-11 10:34:12 -0500

    PROYECTO FINALIZADO

--- empleados/views.py
+++ empleados/views.py
@@ -37,50 +37,41 @@ def registrar(request):
     if request.method == 'POST':
         user_form = CustomUserCreationForm(request.POST, request.FILES, prefix='user')
         if user_form.is_valid():
-            user_form.save()
+            user = user_form.save(commit=False)
+            user.set_password(user_form.cleaned_data['password1'])  # Asegúrate de guardar la contraseña encriptada
+            user.save()
+            messages.success(request, "Usuario creado exitosamente.")
+            return redirect('home_admin')
+        else:
+            messages.error(request, "Hubo un error al crear el usuario. Por favor revisa los datos ingresados.")
+            print(user_form.errors)  # Opcional: para depurar errores en la consola
 
-        return redirect('home_admin')
     else:
         user_form = CustomUserCreationForm(prefix='user')
 
     return render(request, 'registrar_personal.html', {'user_form': user_form})
 
 
-@login_required
 
+@login_required
 def logout_view(request):
     logout(request)
     return redirect('landing')
 
 
 @login_required
-
 def home_admin(request):
-    reservas = Reserva.objects.all()
-    eventos = [
-        {
-
-            "title": f"{reserva.usuario.nombre[0]}. {reserva.usuario.apellido1} {reserva.usuario.apellido2[0]}. ({reserva.turno})",
-            "start": str(reserva.fecha),
-            "color": "red" if reserva.usuario.tipo_usuario == 'coordinador' else "blue",
-            "description": f"Empleado: {reserva.usuario.cedula}, Turno: {reserva.turno}"
-
-        }
-        for reserva in reservas
-    ]
-    return render(request, 'home_admin.html', {'eventos': eventos, 'nombre': request.user.nombre, 'apellido1': request.user.apellido1, 'apellido2': request.user.apellido2,
+    return render(request, 'home_admin.html', {'nombre': request.user.nombre, 'apellido1': request.user.apellido1, 'apellido2': request.user.apellido2,
         'cedula': request.user.cedula})
 
 
 @login_required
-
 def home_empleado(request):
     return render(request, 'home_empleado.html', {'nombre': request.user.nombre, 'apellido1': request.user.apellido1, 'apellido2': request.user.apellido2,
         'cedula': request.user.cedula})
 
 
 @login_required
-
 def usuarios_list(request):
     # Filtrar usuarios que sean coordinadores o empleados
     usuarios = User.objects.filter(tipo_usuario__in=[TipoUsuario.coordinador, TipoUsuario.empleado])
@@ -91,7 +82,6 @@ def usuarios_list(request):
 
 
 @login_required
-
 def eliminar_usuario(request, usuario_id):
     usuario = get_object_or_404(User, id=usuario_id)
     usuario.delete()
@@ -115,13 +105,15 @@ def editar_usuario(request, usuario_id):
 
 @login_required
 def calendario(request):
+    base = ''
+    if request.user.tipo_usuario == 'admin':
+        base = 'base_admin.html'
+    else:
+        base = 'base_empleados.html'
+
     if request.method == 'POST':
         fecha = request.POST.get('fecha')
         turno = request.POST.get('turno')
-
-        if request.user.tipo_usuario == 'admin':
-            messages.warning(request, "No tienes permisos para crear una reserva.")
-            return redirect('calendario')
 
         if not fecha or not turno:
             messages.warning(request, "Selecciona una fecha y un turno.")
@@ -136,12 +128,12 @@ def calendario(request):
             return redirect('calendario_reservas')
 
         # Validación de coordinadores
-        if usuario.tipo_usuario == 'coordinador':
+        if usuario.tipo_usuario == 'coordinador' or usuario.tipo_usuario == 'admin':
             if fecha.weekday() != 4:  # No es viernes
                 messages.error(request, "Los coordinadores solo pueden tomar descansos los viernes.")
                 return redirect('calendario_reservas')
 
-            if Reserva.objects.filter(fecha=fecha, turno=turno, usuario__tipo_usuario='coordinador').exists():
+            if Reserva.objects.filter(fecha=fecha, turno=turno, usuario__tipo_usuario='coordinador').exists() or Reserva.objects.filter(fecha=fecha, turno=turno, usuario__tipo_usuario='admin').exists():
                 messages.error(request, "Ya hay un coordinador en ese turno.")
                 return redirect('calendario_reservas')
 
@@ -160,21 +152,29 @@ def calendario(request):
 
             "title": f"{reserva.usuario.nombre[0]}. {reserva.usuario.apellido1} {reserva.usuario.apellido2[0]}. ({reserva.turno})",
             "start": str(reserva.fecha),
-            "color": "red" if reserva.usuario.tipo_usuario == 'coordinador' else "blue",
+            "color": "green" if reserva.usuario.tipo_usuario == 'admin' else "red" if reserva.usuario.tipo_usuario == 'coordinador' else "blue",
             "description": f"Empleado: {reserva.usuario.cedula}, Turno: {reserva.turno}"
-
     }
+
         for reserva in reservas
     ]
-    return render(request, 'calendario.html', {'eventos': eventos})
+    return render(request, 'calendario.html', {'eventos': eventos, 'base': base})
 
 
+@login_required
 def mis_reservas(request):
-    # Asumiendo que el usuario está autenticado
+    base = ''
+    if request.user.tipo_usuario == 'admin':
+        base = 'base_admin.html'
+    else:
+        base = 'base_empleados.html'
+
     reservas = Reserva.objects.filter(usuario=request.user)
 
-    return render(request, 'mis_reservas.html', {'reservas': reservas})
+    return render(request, 'mis_reservas.html', {'reservas': reservas, 'base': base})
 
+
+@login_required
 def eliminar_reserva(request, id):
     reserva = get_object_or_404(Reserva, id=id)
     reserva.delete()
